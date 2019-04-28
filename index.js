b889a86a487726f60d11a27d61ac5691a17d819d
bouvetisle <archinardv@gmail.com>
2019-04-28 22:17:23 +0100

    Adding title header

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var dice1="images\\dice" + (Math.floor(Math.random()* 6)+1)+".png";
 var dice2="images\\dice" + (Math.floor(Math.random()* 6)+1)+".png";
 document.getElementsByTagName("img")[0].setAttribute("src",dice1);
 document.getElementsByTagName("img")[1].setAttribute("src",dice2);
-var h1=document.querySelector("h1");
+var h2=document.querySelector("h2");
 if(dice1>dice2)
 {
-  h1.innerHTML="Player 1 wins!";
+  h2.innerHTML="Player 1 wins!";
 }
 else if(dice1<dice2){
-  h1.innerHTML="Player 2 wins!";
+  h2.innerHTML="Player 2 wins!";
 }
 else if(dice1==dice2){
-  h1.innerHTML="Draw!";
+  h2.innerHTML="Draw!";
 }
